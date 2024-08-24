1c7f994fa33521092e712410c89b8d2b833c5a35
Adil abraar Syed <129985270+adilsyed003@users.noreply.github.com>
2024-08-24 15:23:42 +0530

    functions

--- intro.js
+++ intro.js
@@ -1,4 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var obj = { name: "adil", age: 19 };
 console.log(obj.name);
 console.log(obj.age);
 console.log("adil 19 years");
+//functions:->
+var addName = function (name, age) {
+    return "Your name is ".concat(name, " and age is ").concat(age);
+};
+console.log(addName(obj.name, obj.age));
--- intro.ts
+++ intro.ts
@@ -2,3 +2,11 @@ let obj = { name: "adil", age: 19 };
 console.log(obj.name);
 console.log(obj.age);
 console.log("adil 19 years");
+
+//functions:->
+
+let addName = (name: string, age: number): string => {
+  return `Your name is ${name} and age is ${age}`;
+};
+console.log(addName(obj.name, obj.age));
+export {};
