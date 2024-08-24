c44b8faf9c49efd6efff88da839968e4ea9bac32
Adil abraar Syed <129985270+adilsyed003@users.noreply.github.com>
2024-08-24 15:42:23 +0530

    objects

--- intro.ts
+++ intro.ts
@@ -9,4 +9,18 @@ let addName = (name: string, age: number): string => {
   return `Your name is ${name} and age is ${age}`;
 };
 console.log(addName(obj.name, obj.age));
+
+//functions and objects -->
+
+let returnFunc = (name: string, age: number): {} => {
+  return { newName: name, newAge: age };
+};
+let returnNewFunc = (
+  name: string,
+  age: number
+): { newName: string; newAge: number } => {
+  return { newName: name, newAge: age };
+};
+console.log(returnFunc(obj.name, obj.age));
+console.log(returnNewFunc(obj.name, obj.age));
 export {};
