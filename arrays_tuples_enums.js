34162cd5a3e21b7d037439ab3f7efe2c536d3785
Adil abraar Syed <129985270+adilsyed003@users.noreply.github.com>
2024-08-24 23:58:33 +0530

    arrays and tuples js file

---
+++ arrays_tuples_enums.js
@@ -0,0 +1,19 @@
+//single type
+var arr = [];
+arr.push("adil");
+//union
+// const array: number[] | string[] = [1,3,"adil"]
+//not possible either all string or all number
+//both num and string
+var arr1 = [1, 2, "adil"];
+//TUPLES -> order of the data
+var user = ["adil", 5, true];
+user.push("adilll"); //HOW?
+//enums - restrict users choice
+var user1;
+(function (user1) {
+    user1[user1["FIRST"] = 5] = "FIRST";
+    user1[user1["SECOND"] = 6] = "SECOND";
+    user1[user1["THIRD"] = 7] = "THIRD";
+})(user1 || (user1 = {}));
+var myChoice = user1.SECOND;
